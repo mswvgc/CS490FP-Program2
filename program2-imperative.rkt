506ca5ed5b3b4906735d9a5b1fea93a0a67a66d5
mswvgc <98501566+mswvgc@users.noreply.github.com>
2023-03-13 10:32:55 -0500

    Update program2-imperative.rkt

--- program2-imperative.rkt
+++ program2-imperative.rkt
@@ -5,9 +5,9 @@
 ;;;;; 03/12/2023
 ;;;;; CS 490 FP Program 2 - imperative version
 
-;;;;; This program currently uses mutable hash tables to store and update word frequencies and differences between author profiles
+;;;;; This program currently uses mutable hash tables
 ;;;;; I am in the process of refactoring the code into a more functional style that avoids the use of mutable data structures and loop constructs
-;;;;; I encountered some issues with correctly implementing the immutable hash tables, so I am including the imperitve version as a fallback solution
+;;;;; I encountered some issues with correctly implementing the immutable hash tables, so I am including the imperitve version as a fallback solution for now
 
 
 ;;; Reads a text file and generates a hash table of word frequencies. 
