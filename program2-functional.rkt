cb893862921ff791f8a21ecc0545abac5a7ceb92
mswvgc <98501566+mswvgc@users.noreply.github.com>
2023-04-03 18:24:35 -0500

    Added comments

--- program2-functional.rkt
+++ program2-functional.rkt
@@ -2,12 +2,10 @@
 
 ;;;;; Michael Wright
 ;;;;; mswvgc@umsystems.edu
-;;;;; 03/12/2023
-;;;;; CS 490 FP Program 2 - imperative version
+;;;;; 4/6/2023
+;;;;; CS 490 FP Program 2 - functional version
 
-;;;;; This program uses a mixture of functional and imperative styles
-;;;;; I am in the process of refactoring the code into a purely functional style that avoids the use of mutable data structures and loop constructs
-;;;;; I encountered some issues with correctly implementing the immutable hash tables, so I am including the imperitve version as a fallback solution for now
+;;;;; This program uses a purely functional style, all hash-tables are immutable
 
 ;;; Reads a text file and generates a hash table of word frequencies. 
 (define (process-file file-path)
